38d2d36b264d5ddb27fb57bbfe9db1c0ee4d1c88
lomidez <49808801+lomidez@users.noreply.github.com>
2022-04-18 15:27:02 +0300

    Update run.py

--- run.py
+++ run.py
@@ -41,7 +41,7 @@ parser.add_argument('--temperature', default=1000, type=float, help='the largest
 parser.add_argument('--nll-oracle', default=False, action='store_true', help='if using nll-oracle metric')
 parser.add_argument('--nll-gen', default=False, action='store_true', help='if using nll-gen metric')
 parser.add_argument('--bleu', default=False, action='store_true', help='if using bleu metric, [2,3,4,5]')
-parser.add_argument('--selfbleu', default=False, action='store_true', help='if using selfbleu metric, [2,3,4,5]')
+parser.add_argument('--selfbleu', default=True, action='store_true', help='if using selfbleu metric, [2,3,4,5]') #########
 parser.add_argument('--doc-embsim', default=False, action='store_true', help='if using DocEmbSim metric')
 
 # relational memory
